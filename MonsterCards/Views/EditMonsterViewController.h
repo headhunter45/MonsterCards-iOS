5f58e9e41d1aefd1eacf9b806efc63c1dc171f4f
Tom Hicks <headhunter3@gmail.com>
2020-09-12 00:07:33 -0700

    Adds a monster edit view.

---
+++ MonsterCards/Views/EditMonsterViewController.h
@@ -0,0 +1,21 @@
+//
+//  EditMonsterViewController.h
+//  MonsterCards
+//
+//  Created by Tom Hicks on 9/8/20.
+//  Copyright © 2020 Tom Hicks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Monster.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EditMonsterViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@property Monster* originalMonster;
+@property (weak, nonatomic) IBOutlet UITableView *monsterTableView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MonsterCards/Views/EditMonsterViewController.m
@@ -0,0 +1,25 @@
+//
+//  EditMonsterViewController.m
+//  MonsterCards
+//
+//  Created by Tom Hicks on 9/8/20.
+//  Copyright © 2020 Tom Hicks. All rights reserved.
+//
+
+#import "EditMonsterViewController.h"
+
+@interface EditMonsterViewController ()
+
+@end
+
+@implementation EditMonsterViewController
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    return nil;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+@end
