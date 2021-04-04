6a5c7b43841122a0e648fad3cdfb564911a716a1
Tom Hicks <headhunter3@gmail.com>
2021-04-04 01:41:41 -0700

    Adds DTO classes to help load a monster file from https://tetra-cube.com/dnd/dnd-statblock.html

---
+++ MonsterCards/Models/TraitDTO.swift
@@ -0,0 +1,42 @@
+//
+//  TraitDTO.swift
+//  MonsterCards
+//
+//  Created by Tom Hicks on 3/28/21.
+//
+
+import Foundation
+
+struct TraitDTO {
+    var name: String
+    var note: String
+    var desc: String
+}
+
+private enum TraitDTOCodingKeys: String, CodingKey {
+    case name = "name"
+    case note = "note"
+    case desc = "desc"
+}
+
+extension TraitDTO: Decodable {
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: TraitDTOCodingKeys.self)
+        self.name = (try? container.decode(String.self, forKey: .name)) ?? ""
+        self.note = (try? container.decode(String.self, forKey: .note)) ?? ""
+        self.desc = (try? container.decode(String.self, forKey: .desc)) ?? ""
+    }
+}
+
+extension TraitDTO: Encodable {
+        
+    func encode(to encoder: Encoder) throws {
+    
+        var container = encoder.container(keyedBy: TraitDTOCodingKeys.self)
+        try container.encode(self.name, forKey: .name)
+        try container.encode(self.note, forKey: .note)
+        try container.encode(self.desc, forKey: .desc)
+    }
+}
